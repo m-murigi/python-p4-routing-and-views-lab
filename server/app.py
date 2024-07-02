a9c8ebfeb077c4a8247137ba848b2203dbd10d54
Emmanuel <zacmac807@gmail.com>
2024-07-02 11:26:41 +0300

    complete task

--- server/app.py
+++ server/app.py
@@ -1,3 +1,47 @@
+# #!/usr/bin/env python3
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+# @app.route('/print/<string:parameter>')
+# def print_string(parameter):
+#     print(parameter)
+#     return (parameter)
+
+# @app.route('/count/<int:parameter>')
+# def count(parameter):
+#     numbers = "/n".join(str(i) for i in range(parameter))
+#     return (numbers)
+
+
+# # @app.route("/count/<int:number>")
+# # def count(number):
+
+
+# @app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+# def math(num1,operation,num2):
+#     if operation == '+':
+#         return (str(num1 + num2))
+#     elif operation == '-':
+#         return (str(num1 - num2))
+#     elif operation == '*':
+#         return (str(num1 * num2))
+#     elif operation == 'div':
+#         return (str(num1 / num2))
+#     elif operation == '%':
+#         return (str(num1 % num2))
+#     else:
+#         return ("Invalid Operation")
+
+
+# if __name__ == '__main__':
+#     app.run(port=5555, debug=True)
+
 #!/usr/bin/env python3
 
 from flask import Flask
@@ -11,33 +55,28 @@ def index():
 @app.route('/print/<string:parameter>')
 def print_string(parameter):
     print(parameter)
-    return (parameter)
+    return parameter
 
 @app.route('/count/<int:parameter>')
 def count(parameter):
-    numbers = "/n".join(str(i) for i in range(parameter))
-    return (numbers)
-
-
-# @app.route("/count/<int:number>")
-# def count(number):
-    
+    numbers = "\n".join(str(i) for i in range(parameter))
+    return f"{numbers}\n"
 
 @app.route('/math/<int:num1>/<string:operation>/<int:num2>')
-def math(num1,operation,num2):
+def math(num1, operation, num2):
     if operation == '+':
-        return (str(num1 + num2))
+        return str(num1 + num2)
     elif operation == '-':
-        return (str(num1 - num2))
+        return str(num1 - num2)
     elif operation == '*':
-        return (str(num1 * num2))
+        return str(num1 * num2)
     elif operation == 'div':
-        return (str(num1 / num2))
+        return str(num1 / num2)
     elif operation == '%':
-        return (str(num1 % num2))
+        return str(num1 % num2)
     else:
-        return ("Invalid Operation")
-
+        return "Invalid Operation"
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
